913024acc55a026e718434b7dd00cc02d40bfc29
Shaw Clifton <shawwc@uw.edu>
2023-03-03 00:06:27 -0800

    second

--- ps6.R
+++ ps6.R
@@ -3,34 +3,50 @@
 library(shiny)
 library(dplyr)
 library(tidyverse)
-read_delim("UAH-lower-troposphere-long.csv.bz2")
+globe = read_delim("UAH-lower-troposphere-long.csv.bz2")
 ui <- fluidPage(
-
     titlePanel("Global Temperature Data"),
-
-    sidebarLayout(
-        sidebarPanel(
-        
-        
-        ),
-
-        mainPanel(
-           plotOutput("distPlot")
-        )
+    
+    mainPanel(
+      tabsetPanel(
+        tabPanel("About", p("This app uses satellite temperature data from ", strong("UAH")),
+                 p("Temperature ", em("temp"), " is measured as deviation (deg C) from 1991-2020 baseline"),
+                 p("The dataset contains ", nrow(globe), " observations and ", ncol(globe), " variables
+                    Here is a small (random) sample of data:"), dataTableOutput("sample")),
+        tabPanel("Plot",
+                 sidebarLayout(sidebarPanel(p("yup")), mainPanel(p("nope")))),
+        tabPanel("Tables",
+                 sidebarLayout(
+                   sidebarPanel(
+                     p("This panel displays average temperature over different time periods: ", 
+                       em("months"), " , ", em("years"), ", and ", em("decades")),
+                     radioButtons("range", label = "Average over:",
+                                  c(""))),
+                        
+                   mainPanel(p("Temperature range ", min(globe$temp), " - ", max(globe$temp)),
+                             tableOutput("tableone"))
+                 )
+      )
+    )
     )
 )
 
 
 server <- function(input, output) {
+  output$sample <- renderDataTable({
+    globe %>%
+      sample_n(5)
+  })
+  
+  
+output$tableone = renderDataTable({
+  globe %>% 
+    
+    arrange(desc())
+})
 
-    output$distPlot <- renderPlot({
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
-    })
+  
+  
 }
 
 shinyApp(ui = ui, server = server)
